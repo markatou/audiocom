5f47a36b747786c48a1eea1f0d55c08eba98962c
Lilika Markatou <markatou@mit.edu>
2016-11-17 13:11:22 -0500

    Cleaned the code a bit

--- audiocom.py
+++ audiocom.py
@@ -25,19 +25,101 @@ import datetime
 
 def run(config):
     s = "active"
-    now = datetime.datetime.now()
+    modulated_samples, channel, sources  = makeChannel(config)
+    time = datetime.datetime.now()
+    
     while s == "active":
-        if (datetime.datetime.now() - now).total_seconds() > 5 + 10*random.random():
-            print((datetime.datetime.now() - now).total_seconds())
-            now = datetime.datetime.now()
-            if s == "active":
-                s = elect(config)
-                print((datetime.datetime.now() - now).total_seconds())
-                print("s =" + s)
+        if (datetime.datetime.now() - time).total_seconds() > 10:
+            time = datetime.datetime.now()
+            if random.random() > 0.5:
+                send(config,modulated_samples,channel,sources)
             else:
-                print("s = " + s)
+                s = listen(config,modulated_samples,channel,sources)
+            print( "s= " + s)
+
+def makeChannel(config):
+    # Create the preamble to pre-pend to the transmission
+    preamble = Preamble(config)
+
+    # Create the sources
+    sources = {}
+    for i in range(len(config.channels)):
+        frequency = config.channels[i]
+        source = Source(config, i)
+        print("Channel: %d Hz" % frequency)
+        print("\n".join(["\t%s" % source]))
+        sources[frequency] = source
+
+    # Create a sender for each source, so we can process the bits to
+    # get the modulated samples.  We combine all of the modulated
+    # samples into a single array by adding them.
+
+    baseband_samples = []
+    modulated_samples = []
+
+    for frequency in sources:
+        src = sources[frequency]
+        sender = Sender(frequency, preamble, config)
+        sender.set_source(src)
+
+        modulated_samples = util.add_arrays(sender.modulated_samples(), modulated_samples)
+        baseband_samples = util.add_arrays(sender.bits_to_samples(src.payload), baseband_samples)
+
+    # Create the channel
+    if config.bypass:
+        channel = AbstractChannel(config.bypass_noise, config.bypass_h, config.bypass_lag)
+    else:
+        channel = AudioChannel(config)
+    return modulated_samples, channel, sources
+
+    
         
-        
+def send(config,modulated_samples, channel,sources):
+
+    # Transmit and receive data on the channel.  The received samples
+    # (samples_rx) have not been processed in any way.
+
+    samples_rx = channel.xmit(modulated_samples)
+    print('Sent the samples')
+
+def listen(config,modulated_samples,channel,sources):
+    preamble = Preamble(config)
+    samples_rx = channel.recv(4*modulated_samples)
+    print('Received', len(samples_rx), 'samples')
+
+    for frequency in config.channels:
+        r = Receiver(frequency, preamble, config)
+        try:
+            # Call the main receiver function.  The returned array of bits
+            # EXCLUDES the preamble.
+            bits  = r.process(samples_rx)
+    
+            # Push into a Sink so we can convert back to a useful payload
+            # (this step will become more useful when we're transmitting
+            # files or images instead of just bit arrays)
+            src = sources[frequency]
+            sink = Sink(src)
+            received_payload = sink.process(bits)
+            print("Received %d data bits" % len(received_payload))
+            if src.type == Source.TEXT:
+                print("Received text was:", sink.received_text)
+                if ((str(sink.received_text)) == "Mens et manus.\n"):
+                    return "inactive"
+ 
+            if len(received_payload) > 0:
+                # Output BER
+                hd = util.hamming(received_payload, src.payload)
+                ber = float(hd)/len(received_payload)
+                print('BER:', ber)
+            else:
+                print('Could not recover transmission.')
+
+        except Exception as e:
+            print(repr(e))
+    return "active"
+
+
+
 
 
 def elect(config):
