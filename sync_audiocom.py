6b82fe4cd3a1b5ffe27afe75e25ea69f8e23a93e
Carlos Caldera <ccaldera@mit.com>
2016-11-21 17:46:40 -0500

    Made sync_audiocom listen for ping to start

--- sync_audiocom.py
+++ sync_audiocom.py
@@ -23,14 +23,27 @@ from sink import Sink
 from source import Source
 import datetime
 
+from socket import *
+host = ""
+port = 13000
+buf = 1024
+
 def run(config):
     modulated_samples, channel, sources  = makeChannel(config)
 
     # Listen for start signal
     print("Listening for start signal...")
     start = False
+
+    addr = (host, port)
+    UDPSock = socket(AF_INET, SOCK_DGRAM)
+    UDPSock.bind(addr)
     while not start:
-        start = listen(config, 20*modulated_samples, channel, sources, "Mens et manus.\n")
+        (data, addr) = UDPSock.recvfrom(buf)
+        print("Received message", data)
+        if data == b"Start":
+            start = True
+    UDPSock.close()
 
     print("Starting leader election")
 
@@ -52,14 +65,6 @@ def run(config):
             print("Active ? : <%s>" % (active))
             print("Round Number : <%d>" % (round_number))
             round_number += 1
-
-def send_start(config):
-    modulated_samples, channel, sources  = makeChannel(config)
-    print("Sending start signal!!")
-
-    for i in range(5):
-        time.sleep(5)
-        send(config, modulated_samples, channel, sources)
 
 def makeChannel(config):
     # Create the preamble to pre-pend to the transmission
@@ -145,85 +150,7 @@ def listen(config, modulated_samples, channel, sources, message):
             print(repr(e))
     return False
 
-def elect(config):
-    '''Primary Audiocom functionality.'''
-
-    # Create the preamble to pre-pend to the transmission
-    preamble = Preamble(config)
-
-    # Create the sources
-    sources = {}
-    for i in range(len(config.channels)):
-        frequency = config.channels[i]
-        source = Source(config, i)
-        print("Channel: %d Hz" % frequency)
-        print("\n".join(["\t%s" % source]))
-        sources[frequency] = source
-
-    # Create a sender for each source, so we can process the bits to
-    # get the modulated samples.  We combine all of the modulated
-    # samples into a single array by adding them.
-
-    baseband_samples = []
-    modulated_samples = []
-
-    for frequency in sources:
-        src = sources[frequency]
-        sender = Sender(frequency, preamble, config)
-        sender.set_source(src)
-
-        modulated_samples = util.add_arrays(sender.modulated_samples(), modulated_samples)
-        baseband_samples = util.add_arrays(sender.bits_to_samples(src.payload), baseband_samples)
-
-    # Create the channel
-    if config.bypass:
-        channel = AbstractChannel(config.bypass_noise, config.bypass_h, config.bypass_lag)
-    else:
-        channel = AudioChannel(config)
-
-    # Transmit and receive data on the channel.  The received samples
-    # (samples_rx) have not been processed in any way.
-
-    if random.random() > 0.5:
-        samples_rx = channel.xmit(modulated_samples)
-        print('Sent', len(samples_rx), 'samples')
-    else:
-    
-        samples_rx = channel.recv(4*modulated_samples)
-        print('Received', len(samples_rx), 'samples')
-
-        for frequency in config.channels:
-            r = Receiver(frequency, preamble, config)
-            try:
-                # Call the main receiver function.  The returned array of bits
-                # EXCLUDES the preamble.
-                bits  = r.process(samples_rx)
-    
-                # Push into a Sink so we can convert back to a useful payload
-                # (this step will become more useful when we're transmitting
-                # files or images instead of just bit arrays)
-                src = sources[frequency]
-                sink = Sink(src)
-                received_payload = sink.process(bits)
-                print("Received %d data bits" % len(received_payload))
-                if src.type == Source.TEXT:
-                    print("Received text was:", sink.received_text)
-                    if ((str(sink.received_text)) == "Mens et manus.\n"):
-                        return "inactive"
- 
-                if len(received_payload) > 0:
-                    # Output BER
-                    hd = util.hamming(received_payload, src.payload)
-                    ber = float(hd)/len(received_payload)
-                    print('BER:', ber)
-                else:
-                    print('Could not recover transmission.')
-
-            except Exception as e:
-                print(repr(e))
-    return "active"
-    
-if __name__ == '__main__':
+def start():
 
     parser = argparse.ArgumentParser()
 
@@ -246,7 +173,7 @@ if __name__ == '__main__':
     parser.add_argument("-L", "--avgwindow", type=int, help="window for averaging filter")
 
     parser.add_argument("--file", type=str, action="append", help="filename(s)")
-    parser.add_argument("--start", action='store_true', help="send only start signal")
+    #parser.add_argument("--start", action='store_true', help="send only start signal")
 
     # Modulation (signaling) and Demodulation options
     parser.add_argument("-k", "--keytype", type=str, default="on_off", help="keying (signaling) scheme")
@@ -271,8 +198,4 @@ if __name__ == '__main__':
     config = Config(args)
     print(config) # useful output
 
-    if args.start:
-        send_start(config)
-    else:
-        # Go!
-        run(config)
+    run(config)
